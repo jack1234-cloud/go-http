9db802f635eb2c03608135733325e680a3c2e682
joflynn <jack.oflynn@aig.com>
2022-02-17 14:07:01 +0000

    updates

--- videos/main.go
+++ videos/main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/", HandleGetVideos)
+	http.HandleFunc("/update", HandleUpdateVideos)
+
 	http.ListenAndServe(":8080", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
+func HandleGetVideos(w http.ResponseWriter, r *http.Request) {
 
+	videos := getVideos()
+
+	videoBytes, err := json.Marshal(videos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write(videoBytes)
+}
+
+func HandleUpdateVideos(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		var videos []video
+		err = json.Unmarshal(body, &videos)
+		if err != nil {
+			w.WriteHeader(400)
+			fmt.Fprintf(w, "Bad request")
+		}
+
+		saveVideos(videos)
+
+	} else {
+		w.WriteHeader(405)
+		fmt.Fprintf(w, "Method not Supported!")
+	}
 }
